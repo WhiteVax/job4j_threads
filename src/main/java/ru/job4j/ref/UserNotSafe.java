85027261dc7a8e88fbbb0e15151dced378b12183
WhiteVax <WhiteVax@gmail.com>
2022-09-30 21:52:19 +0300

    4. Thread без общих ресурсов

---
+++ src/main/java/ru/job4j/ref/UserNotSafe.java
@@ -0,0 +1,21 @@
+package ru.job4j.ref;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserNotSafe {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), user);
+    }
+
+    public User findById(int id) {
+        return users.get(id);
+    }
+}
+
