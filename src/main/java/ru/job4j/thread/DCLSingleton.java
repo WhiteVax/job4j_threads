d3d450c6e61c663efa96902d0ddd4001e382c8c1
WhiteVax <WhiteVax@gmail.com>
2022-09-30 19:06:43 +0300

    2. Модель памяти Java

---
+++ src/main/java/ru/job4j/thread/DCLSingleton.java
@@ -0,0 +1,24 @@
+package ru.job4j.thread;
+
+/**
+ * Использование общей области памяти
+ */
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+}
