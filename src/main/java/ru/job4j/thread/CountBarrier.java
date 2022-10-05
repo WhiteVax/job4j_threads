0e207f32f6c85beb1ccadd953b4a0254f8cde24d
WhiteVax <WhiteVax@gmail.com>
2022-10-05 22:01:58 +0300

    0. Управление нитью через wait.

--- src/main/java/ru/job4j/thread/CountBarrier.java
+++ src/main/java/ru/job4j/thread/CountBarrier.java
@@ -17,7 +17,7 @@ public class CountBarrier {
     }
 
     public synchronized void await() {
-        while (count >= total) {
+        while (count < total) {
             try {
                 monitor.wait();
             }  catch (InterruptedException e) {
